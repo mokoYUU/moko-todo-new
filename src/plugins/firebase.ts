e510016de66b3f9f289447f47a7bd4e5f9be56d1
uu y <uuy@Gavumac.local>
2024-08-29 17:42:56 +0900

    Refactor file structure and update nuxt.config.ts

--- src/plugins/firebase.ts
+++ src/plugins/firebase.ts
@@ -1,7 +1,8 @@
 // plugins/firebase.ts
 // 認証サービス(auth)+データベース(db)を全体で使う
 import { defineNuxtPlugin } from '#app'
-import { auth, db } from '~/firebaseConfig'
+import { auth, db } from '../firebaseConfig'
+
 
 export default defineNuxtPlugin((nuxtApp) => {
   if (!nuxtApp.vueApp.config.globalProperties.$auth) {
